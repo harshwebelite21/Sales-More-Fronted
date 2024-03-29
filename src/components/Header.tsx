513330078639688fc906341d74b4d6cb7cc8267f
Harsh Vaghasiya <harshvaghasiya@webelight.co.in>
2024-03-29 15:33:53 +0530

    section-changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,78 +4,27 @@ import { FiShoppingCart } from "react-icons/fi";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+type MenuItem = {
+  to: string;
+  label: string | JSX.Element;
+};
+
+const Header: React.FC = () => {
   const [click, setClick] = useState(true);
-  const content = (
-    <>
-      <div className="md:hidden lg:hidden block absolute w-full mt-7 left-0 right-0 bg-slate-900 transition">
-        <ul className="text-xl py-12 px-20 flex-col text-white text-center">
-          <Link
-            to="/"
-            onClick={() => {
-              setClick(!click);
-            }}
-          >
-            <li
-              className={`cursor-pointer my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded`}
-            >
-              Home
-            </li>
-          </Link>
-          <Link
-            to="/about"
-            onClick={() => {
-              setClick(!click);
-            }}
-          >
-            <li
-              className={`cursor-pointer my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded`}
-            >
-              About
-            </li>
-          </Link>
-          <Link
-            to="/products"
-            onClick={() => {
-              setClick(!click);
-            }}
-          >
-            <li
-              className={`cursor-pointer my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded`}
-            >
-              Products
-            </li>
-          </Link>
-          <Link
-            to="/contact"
-            onClick={() => {
-              setClick(!click);
-            }}
-          >
-            <li
-              className={`cursor-pointer my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded`}
-            >
-              Contact
-            </li>
-          </Link>
-          <Link
-            to="/cart"
-            onClick={() => {
-              setClick(!click);
-            }}
-          >
-            <li
-              className={`cursor-pointer my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded flex justify-center`}
-            >
-              <FiShoppingCart className="size-8 " />
-            </li>
-          </Link>
-        </ul>
-      </div>
-    </>
-  );
+
+  const menuItems: MenuItem[] = [
+    { to: "/", label: "Home" },
+    { to: "/about", label: "About" },
+    { to: "/products", label: "Products" },
+    { to: "/contact", label: "Contact" },
+    { to: "/cart", label: <FiShoppingCart className="size-8" /> },
+  ];
+  const handleMenuClick = () => {
+    setClick(!click);
+  };
+
   return (
-    <div className="flex justify-between px-10 py-5 text-lg">
+    <div className="flex justify-between px-4 md:px-10 py-5 text-lg">
       <div className="items-center justify-start">
         <Link to="/">
           <img
@@ -87,41 +36,15 @@ const Header = () => {
       </div>
       <div className="justify-end items-center self-center">
         <ul className="hidden md:flex gap-10 items-center self-center">
-          <Link to="/">
-            <li
-              className={`cursor-pointer text-custom-gray hover:text-dynamic-blue  transition duration-300 ease-in-out`}
-            >
-              Home
-            </li>
-          </Link>
-          <Link to="/about">
-            <li
-              className={`cursor-pointer text-custom-gray hover:text-dynamic-blue  transition duration-300 ease-in-out`}
-            >
-              About
-            </li>
-          </Link>
-          <Link to="/products">
-            <li
-              className={`cursor-pointer text-custom-gray hover:text-dynamic-blue  transition duration-300 ease-in-out`}
-            >
-              Products
-            </li>
-          </Link>
-          <Link to="/contact">
-            <li
-              className={`cursor-pointer text-custom-gray hover:text-dynamic-blue  transition duration-300 ease-in-out`}
-            >
-              Contact
-            </li>
-          </Link>
-          <Link to="/cart">
-            <li
-              className={`cursor-pointer text-custom-gray hover:text-dynamic-blue  transition duration-300 ease-in-out`}
-            >
-              <FiShoppingCart className="size-8" />
-            </li>
-          </Link>
+          {menuItems.map((menuItem) => (
+            <Link to={menuItem.to} key={menuItem.to}>
+              <li
+                className={`cursor-pointer text-custom-gray hover:text-dynamic-blue transition duration-300 ease-in-out`}
+              >
+                {menuItem.label}
+              </li>
+            </Link>
+          ))}
         </ul>
 
         <div className="flex md:hidden items-center">
@@ -143,7 +66,32 @@ const Header = () => {
             />
           )}
         </div>
-        <div>{click && content}</div>
+        <div>
+          {click && (
+            <div className="md:hidden lg:hidden block fixed w-full h-[100dvh] mt-7 left-0 right-0 bg-slate-900 transition">
+              <ul className="text-xl py-12 px-20 flex-col text-white text-center">
+                {menuItems.map((menuItem) => (
+                  <Link
+                    to={menuItem.to}
+                    key={menuItem.to}
+                    onClick={handleMenuClick}
+                  >
+                    <li
+                      className={`cursor-pointer my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded ${
+                        typeof menuItem.label === "object" &&
+                        menuItem.label.type === FiShoppingCart
+                          ? "flex justify-center"
+                          : ""
+                      }`}
+                    >
+                      {menuItem.label}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
