6cd668c93bb407fd5dcead243449b499f1de8b59
Harsh Vaghasiya <harshvaghasiya@webelight.co.in>
2024-04-05 10:33:06 +0530

    coupon setting

--- src/modules/ShoppingCart/Components/CartCalculations.tsx
+++ src/modules/ShoppingCart/Components/CartCalculations.tsx
@@ -2,7 +2,7 @@ import { Button, TextField } from "@mui/material";
 
 const CartCalculations = () => {
   return (
-    <div className="w-full rounded-md my-3  border-2 p-5 sticky top-24  flex flex-col gap-7 ">
+    <div className="w-full rounded-md my-3  border-2 p-5 sticky top-24  flex flex-col gap-2 ">
       <div className="font-bold text-3xl">Cart Totals</div>
       <div className="flex justify-between ">
         <h3 className="text-bold text-lg">Subtotal</h3>
@@ -14,17 +14,29 @@ const CartCalculations = () => {
         <div className="text-bold text-lg">$431</div>
       </div>
       <hr />
+      <div className="grid grid-cols-3 items-end w-full">
+        <p className="col-span-1">Apply Coupon</p>
+        <TextField
+          id="standard-basic"
+          label="Enter Here"
+          variant="standard"
+          className="col-span-2"
+        />
+      </div>
+      <hr />
+      <div className="flex justify-between ">
+        <h3 className="text-bold text-sm">Reduction</h3>
+        <div className="text-bold text-lg">-$431</div>
+      </div>
+      <hr />
       <div className="flex justify-between ">
         <h3 className="text-bold text-sm">Order Total</h3>
         <div className="text-bold text-lg">$431</div>
       </div>
       <hr />
-      <div className="flex items-center gap-2">
-        <label htmlFor="coupon">Apply Coupon</label>
-        <TextField id="standard-basic" label="Enter Hear" variant="standard" />
-      </div>
+
       <div className="w-full flex-center ">
-        <Button variant="contained" className="w-[80%] mx-auto">
+        <Button variant="contained" className="w-[80%] mx-auto ">
           CheckOut
         </Button>
       </div>
