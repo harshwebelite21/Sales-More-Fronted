59d2df5a6e64c32b5d61ca726838fd979a48fba1
Harsh Vaghasiya <harshvaghasiya@webelight.co.in>
2024-04-11 13:38:18 +0530

    Bugs solved

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,5 +1,5 @@
 const appConfig = {
-  authToken: import.meta.env.AUTHTOKEN,
+  authToken: import.meta.env.VITE_AUTH_TOKEN,
+  baseUrl: import.meta.env.VITE_BASE_URL,
 };
-
 export default appConfig;
