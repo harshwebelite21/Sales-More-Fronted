162208e497802339554012327676eff0f505f24f
Harsh Vaghasiya <harshvaghasiya@webelight.co.in>
2024-04-15 14:48:29 +0530

    Cart Banner Complete

--- src/modules/ShoppingCart/ShoppingCart.tsx
+++ src/modules/ShoppingCart/ShoppingCart.tsx
@@ -1,4 +1,5 @@
 import { useCartContext } from "../../Context/CartContext";
+import CartBanner from "./Components/CartBanner";
 import CartCalculations from "./Components/CartCalculations";
 import CartItems from "./Components/CartItems";
 import CartMobileItems from "./Components/CartMobileItems";
@@ -9,14 +10,23 @@ const ShoppingCart = () => {
   return (
     <>
       <div className="flex-col w-[90%] mx-auto relative hidden md:flex">
+        <div className="w-full  h-auto">
+          <CartBanner />
+        </div>
+        <hr />
         <div className="w-full my-4 h-auto max-h-[500px] overflow-y-auto">
           <CartItems cart={cart} />
         </div>
+        <hr />
+
         <div className="w-full flex justify-end my-4">
           <CartCalculations />
         </div>
       </div>
       <div className="md:hidden flex-col">
+        <div className="w-[90%] mx-auto my-4">
+          <CartBanner />
+        </div>
         <div className="w-[90%] mx-auto my-4">
           <CartMobileItems cart={cart} />
         </div>
