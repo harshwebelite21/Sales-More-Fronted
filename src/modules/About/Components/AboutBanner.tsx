bfa8354165994b3d4554a1b6de551f8f5d3fe7a6
Harsh Vaghasiya <harshvaghasiya@webelight.co.in>
2024-04-16 16:15:45 +0530

    About Page Complete

---
+++ src/modules/About/Components/AboutBanner.tsx
@@ -0,0 +1,21 @@
+import about_bg from "../../../images/about_bg.jpg";
+import "./style/about.css";
+
+const AboutBanner = () => {
+  return (
+    <div className="relative w-full h-screen">
+      <img src={about_bg} alt="" className="h-[88dvh] w-full object-cover" />
+      <div className="absolute left-[15%] top-[30%] flex flex-col gap-4 flex-center">
+        <h1
+          className="text-4xl md:text-5xl lg:text-6xl"
+          style={{ fontFamily: "Libre Baskerville" }}
+        >
+          About Products
+        </h1>
+        <button className="explore-button">Explore Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default AboutBanner;
